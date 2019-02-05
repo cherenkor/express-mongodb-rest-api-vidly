ca3a33346eebb80477d2e6f0c12437d235354fd3
Kucherenko Sergey <kucherenko.web@gmail.com>
2019-02-05 22:40:42 +0200

    Add users route

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ const genres = require('./routes/genres');
 const movies = require('./routes/movies');
 const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 
-mongoose.connect('mongodb://localhost:27017/vidly', { useNewUrlParser: true, useFindAndModify: false })
+mongoose.connect(
+    'mongodb://localhost:27017/vidly',
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        'useCreateIndex': true
+    })
     .then(() => console.log('Connected to the MongoDB...'))
     .catch(() => console.error('Could not connect to the MongoDB...'))
 
@@ -23,5 +30,6 @@ app.use('/api/genres', genres);
 app.use('/api/movies', movies);
 app.use('/api/customers', customers);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
