1501d538f68fc77f8f8fd05d01b1111fa0c78b3c
Kucherenko Sergey <kucherenko.web@gmail.com>
2019-02-11 10:07:00 +0200

    Implement Single Responsibility principle and split index.js code for modules

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-require("express-async-errors");
 const winston = require("winston");
-const config = require("config");
-const Joi = require("Joi");
-Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+require("./startup/validation")();
+require("./startup/config")();
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 
-if (!config.get("jsonPrivateKey")) {
-  console.error("FATAL ERROR: jsonPrivateKey does not set up");
-  process.exit(1);
-}
-
-winston.add(
-  new winston.transports.File({
-    filename: "logs/logfile.log",
-    handleExceptions: true
-  })
-);
-
-process.on("unhandledRejection", ex => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => winston.info(`Server listening on port ${PORT}`));
